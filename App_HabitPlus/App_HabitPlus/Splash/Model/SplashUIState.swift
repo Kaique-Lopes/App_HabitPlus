e8457a3a9104dc1b4cd11bad9ba21e7624c7fddc
Kaique <kaique.lopes3271@gmail.com>
2021-09-18 19:39:54 -0300

    SplashView

---
+++ App_HabitPlus/App_HabitPlus/Splash/Model/SplashUIState.swift
@@ -0,0 +1,16 @@
+//
+//  SplashUIState.swift
+//  App_HabitPlus
+//
+//  Created by Kaique Lopes de Oliveira on 18/09/21.
+//
+
+import Foundation
+
+enum SplashUIState {
+    case loading
+    case goToSignInScreen
+    case goToHomeScreen
+    case error(String)
+}
+
--- App_HabitPlus/App_HabitPlus/Splash/View/SplashView.swift
+++ App_HabitPlus/App_HabitPlus/Splash/View/SplashView.swift
@@ -8,10 +8,24 @@
 import SwiftUI
 
 struct SplashView: View {
+    
+    @State var state: SplashUIState = .loading
+    
     var body: some View {
-        Text("Ola")
+        switch state {
+        case .loading:
+            Text("Loading")
+        case .goToSignInScreen:
+            Text("Tela Login")
+        case .goToHomeScreen:
+            Text("Tela Principal")
+        case .error(let msg):
+            Text("Error \(msg)")
+        }
     }
 }
+
+// MARK: - Criando o Preview
 struct SplashView_Previews: PreviewProvider {
     static var previews: some View {
         SplashView()
