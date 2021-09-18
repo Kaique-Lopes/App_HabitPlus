1486309f48ce91783bf5e84d0e14e02ca953608b
Kaique <kaique.lopes3271@gmail.com>
2021-09-18 19:21:50 -0300

    Fist Commit

---
+++ App_HabitPlus/App_HabitPlus/App_HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_HabitPlusApp.swift
+//  App_HabitPlus
+//
+//  Created by Kaique Lopes de Oliveira on 18/09/21.
+//
+
+import SwiftUI
+
+@main
+struct App_HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
